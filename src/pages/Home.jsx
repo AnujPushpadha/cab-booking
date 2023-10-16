eba1d581d4b950fb7fc6e721307849545b136cd0
Anuj <pushpadhaanuj@gmail.com>
2023-10-16 11:39:59 +0530

    third commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import data from "../data.json";
-import Search from "./search";
+
 // import FindCoordinates from "../components/FindCoordinates";
 
 const Home = () => {
