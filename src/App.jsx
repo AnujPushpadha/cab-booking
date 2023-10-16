3a59c7395a46b71b3d353bb727580ba7634784c0
Anuj <pushpadhaanuj@gmail.com>
2023-10-16 11:37:49 +0530

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Home from "./pages/home";
-import Search from "./pages/search";
+import Home from "./pages/Home";
+import Search from "./pages/Search";
 import Booking from "./pages/Booking";
 
 const router = createBrowserRouter([
